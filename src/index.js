38166b77d9c4215e8c730720cc697e7b961a8f5f
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-05-01 00:58:52 +0300

    add load more btn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// key - твой уникальный ключ доступа к API.
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-//     safesearch - фильтр по возрасту.Задай значение true
+import renderCardTpl from './templates/render-card.hbs';
+import { Notify } from 'notiflix';
 const refs = {
     searchField: document.body.querySelector('.search-form__input'),
     formEl: document.body.querySelector('.search-form'),
     gallery: document.body.querySelector('.gallery'),
-
+    loadBtn: document.body.querySelector('.load-more'),
 }
 
 let searchFieldValue = null; 
@@ -19,76 +16,63 @@ const SAFESEARCH = 'true';
 const BASE_URL = 'https://pixabay.com/api/';
 
 const searchParams = new URLSearchParams({
-    key: KEY,
-    q: searchFieldValue,
-    image_type: IMAGE_TYPE,
-    orientation: ORIENTATION,
-    safesearch: SAFESEARCH,
+  key: KEY,
+  q: searchFieldValue,
+  image_type: IMAGE_TYPE,
+  orientation: ORIENTATION,
+  safesearch: SAFESEARCH,
+  page: 1,
+  per_page: 20, 
 })
 
 // При нажатии на кнопку поиска вызывается функция onSubmit.
 refs.formEl.addEventListener('submit', onSubmit);
 
-// Функция изменения значения поля поиска в параметрах запроса. Возвращает обновленные параметры в виде JSON-строки.
-function requestGeneration() {;
-    const searchFieldValue = refs.searchField.value;
-    searchParams.set('q', searchFieldValue);
+// Функция изменения значения поля поиска в параметрах запроса. Возвращает объект параметров в виде JSON-строки.
+function requestGeneration() {
+  const searchFieldValue = refs.searchField.value;
+  let count = Number(searchParams.get('page'));
+
+  if (searchParams.get('q') === searchFieldValue) {
+    count += 1;
+
+    searchParams.set('page', count);
     return searchParams.toString();
+  }
+  searchParams.set('q', searchFieldValue);
+  searchParams.set('page', 1);
+  return searchParams.toString();
 }
-// Функция запроса в базу данных. Возвращает промис с данными в JSON-формате 
+
+// Функция запроса в базу данных. Возвращает промис с данными(найденные объекты) в JSON-формате 
 async function getData() {
-    const response = await fetch(`${BASE_URL}?${searchParams}`);
+    const message = "Sorry, there are no images matching your search query. Please try again.";
+    const response = await fetch(`${BASE_URL}?${searchParams}`);;
     const data = await response.json();
-    const images = data.hits;
+    const numbersOfImages = data.totalHits;
+    numbersOfImages === 0 
+    ? Notify.failure(message) : numbersOfImages === 1 ? Notify.success(`You are lucky! We found 1 image.`) : Notify.success(`We found ${numbersOfImages} images.`);
+    const images = await data.hits;
     return images;
 }
 
+// Инициализация функции сабмита поиска
 async function onSubmit(event) {
     event.preventDefault();
-    requestGeneration();
-
+  requestGeneration();
+  
     try {
-        const images = await getData();
-        renderImages(images);
+      const images = await getData();
+      renderImages(images);
+      refs.loadBtn.classList.remove('is-hidden');
     } catch (error) {
-        console.log('error :>> ', error);
+        Notify.failure(error.name);
     }
-    // renderImages(arrayOfImagesData);
 }
 
+// Функция отрисовки изображений в галерее. Принимает массив объектов изображений. Результат - разметка галереи.
 function renderImages(arrayOfImagesData) {
-    const markup = arrayOfImagesData.map(image => {
-        return `<div class="photo-card">
-  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes${image.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views${image.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments${image.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads${image.downloads}</b>
-    </p>
-  </div>
-</div>`;
-    }).join('');
+  refs.gallery.innerHTML = '';
+    const markup = arrayOfImagesData.map(image => renderCardTpl({image})).join('');
     refs.gallery.insertAdjacentHTML('afterbegin', markup);
 }
-
-
-
-// const images = data.hits;
-//     const imagesContainer = document.body.querySelector('.images-container');
-//     imagesContainer.innerHTML = '';
-//     images.forEach(image => {
-//         const imageElement = document.createElement('img');
-//         imageElement.src = image.previewURL;
-//         imageElement.alt = image.tags;
-//         imagesContainer.append(imageElement);
-//     });
-
-// final result
