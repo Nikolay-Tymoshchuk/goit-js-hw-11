08fa21e06c76211319cbddca98cc670253b7faa1
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-05-01 17:04:44 +0300

    add liseners to buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import renderCardTpl from './templates/render-card.hbs';
 import { Notify } from 'notiflix';
+import NewsApiService from './js/api-service';
 const refs = {
     searchField: document.body.querySelector('.search-form__input'),
     formEl: document.body.querySelector('.search-form'),
@@ -7,72 +8,76 @@ const refs = {
     loadBtn: document.body.querySelector('.load-more'),
 }
 
-let searchFieldValue = null; 
+const myApiService = new NewsApiService();
 
-const KEY = '27097192-0671b14f562b4984cf83e0bbe';
-const IMAGE_TYPE = 'photo';
-const ORIENTATION = 'horizontal';
-const SAFESEARCH = 'true';
-const BASE_URL = 'https://pixabay.com/api/';
-
-const searchParams = new URLSearchParams({
-  key: KEY,
-  q: searchFieldValue,
-  image_type: IMAGE_TYPE,
-  orientation: ORIENTATION,
-  safesearch: SAFESEARCH,
-  page: 1,
-  per_page: 20, 
-})
 
 // При нажатии на кнопку поиска вызывается функция onSubmit.
 refs.formEl.addEventListener('submit', onSubmit);
+refs.loadBtn.addEventListener('click', onLoadMore);
 
-// Функция изменения значения поля поиска в параметрах запроса. Возвращает объект параметров в виде JSON-строки.
-function requestGeneration() {
-  const searchFieldValue = refs.searchField.value;
-  let count = Number(searchParams.get('page'));
 
-  if (searchParams.get('q') === searchFieldValue) {
-    count += 1;
 
-    searchParams.set('page', count);
-    return searchParams.toString();
-  }
-  searchParams.set('q', searchFieldValue);
-  searchParams.set('page', 1);
-  return searchParams.toString();
+// Функция уведомлений, в зависимости от количества найденных изображений. 
+function notificationByFetchedResults(lengthOfResultedArray) {
+  const messageIfNotFounded = "Sorry, there are no images matching your search query. Please try again.";
+  const messageIfFoundOne = `You are lucky! We found 1 image.`;
+  const messaheIfFoundedMany = `We found ${lengthOfResultedArray} images.`;
+
+  if (lengthOfResultedArray === 0) {
+    Notify.failure(messageIfNotFounded);
+  } else if (lengthOfResultedArray === 1) {
+    Notify.success(messageIfFoundOne);
+  } else {
+    Notify.success(messaheIfFoundedMany);
+  } 
 }
 
-// Функция запроса в базу данных. Возвращает промис с данными(найденные объекты) в JSON-формате 
-async function getData() {
-    const message = "Sorry, there are no images matching your search query. Please try again.";
-    const response = await fetch(`${BASE_URL}?${searchParams}`);;
-    const data = await response.json();
-    const numbersOfImages = data.totalHits;
-    numbersOfImages === 0 
-    ? Notify.failure(message) : numbersOfImages === 1 ? Notify.success(`You are lucky! We found 1 image.`) : Notify.success(`We found ${numbersOfImages} images.`);
-    const images = await data.hits;
-    return images;
-}
 
 // Инициализация функции сабмита поиска
-async function onSubmit(event) {
-    event.preventDefault();
-  requestGeneration();
-  
-    try {
-      const images = await getData();
-      renderImages(images);
-      refs.loadBtn.classList.remove('is-hidden');
-    } catch (error) {
-        Notify.failure(error.name);
-    }
-}
+
 
 // Функция отрисовки изображений в галерее. Принимает массив объектов изображений. Результат - разметка галереи.
 function renderImages(arrayOfImagesData) {
-  refs.gallery.innerHTML = '';
     const markup = arrayOfImagesData.map(image => renderCardTpl({image})).join('');
-    refs.gallery.insertAdjacentHTML('afterbegin', markup);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
+
+
+async function onSubmit(event) {
+  event.preventDefault();
+  refs.gallery.innerHTML = '';
+  refs.loadBtn.classList.add('is-hidden');
+
+  try {
+    if (myApiService.query !== event.currentTarget.elements.searchQuery.value) {
+      myApiService.query = event.currentTarget.elements.searchQuery.value;
+      myApiService.page = 1;
+      await validationOfOucomingResult(myApiService, true);
+    } else { 
+      myApiService.page += 1;
+      await validationOfOucomingResult(myApiService);
+    }
+    
+  }
+  catch (error) {
+    Notify.failure(error.name);
+  }
+}
+
+async function onLoadMore() {
+  try {
+    myApiService.page += 1;
+    const fetchingRequest = await myApiService.fetchData();
+    renderImages(fetchingRequest.hits);
+  } catch (error) {
+    Notify.failure(error.name);
+  } 
+}
+
+async function validationOfOucomingResult(data, notify) {
+  const fetchingRequest = await data.fetchData();      
+  renderImages(fetchingRequest.hits);
+  fetchingRequest.totalHits > data.per_page && refs.loadBtn.classList.remove('is-hidden');
+  notify && notificationByFetchedResults(fetchingRequest.totalHits);
+}
+
---
+++ src/js/api-service.js
@@ -0,0 +1,59 @@
+export default class NewsApiService {
+  constructor() {
+        this.searchQuery = '';
+        this.KEY = '27097192-0671b14f562b4984cf83e0bbe';
+        this.IMAGE_TYPE = 'photo';
+        this.ORIENTATION = 'horizontal';
+        this.SAFESEARCH = 'true';
+        this.BASE_URL = 'https://pixabay.com/api/';
+        this.page = 1;
+        this.per_page = 20;
+    }
+    
+    getOptions() {
+        const searchParams = new URLSearchParams({
+            key: this.KEY,
+            q: this.searchQuery,
+            image_type: this.IMAGE_TYPE,
+            orientation: this.ORIENTATION,
+            safesearch: this.SAFESEARCH,
+            page: this.page,
+            per_page: this.per_page
+        });
+
+        return searchParams;
+    }     
+
+    async fetchData() {
+        const response = await fetch(`${this.BASE_URL}?${this.getOptions()}`);
+        const data = await response.json();
+        return data;
+    }
+    
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(value) {
+        this.searchQuery = value;
+    }
+
+
+
+    
+//   get(url) {
+//     return this.apiService.get(url);
+//   }
+
+//   post(url, data) {
+//     return this.apiService.post(url, data);
+//   }
+
+//   put(url, data) {
+//     return this.apiService.put(url, data);
+//   }
+
+//   delete(url) {
+//     return this.apiService.delete(url);
+//   }
+}
