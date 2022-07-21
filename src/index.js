89b9fd4ec041dc60ffcc82941e381ee515024f48
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-07-21 12:28:16 +0300

    reset path

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import renderCardTpl from './templates/render-card.hbs';
 import { Notify } from 'notiflix';
 import NewsApiService from './js/api-service';
 import MyError from './js/myError';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const refs = {
-    searchField: document.body.querySelector('.search-form__input'),
-    formEl: document.body.querySelector('.search-form'),
-    gallery: document.body.querySelector('.gallery'),
-    loadBtn: document.body.querySelector('.load-more'),
-}
-Notify.init({clickToClose: true});
+  searchField: document.body.querySelector('.search-form__input'),
+  formEl: document.body.querySelector('.search-form'),
+  gallery: document.body.querySelector('.gallery'),
+  loadBtn: document.body.querySelector('.load-more'),
+};
+Notify.init({ clickToClose: true });
 const myApiService = new NewsApiService();
-
 
 // При нажатии на кнопку поиска вызывается функция onSubmit.
 refs.formEl.addEventListener('submit', onSubmit);
@@ -20,23 +19,21 @@ refs.loadBtn.addEventListener('click', onLoadMore);
 
 // Функция поиска последней страницы по данному запросу.
 
-const findLastPage = (value) => Math.ceil(value.totalHits / myApiService.per_page);
+const findLastPage = value => Math.ceil(value.totalHits / myApiService.per_page);
 
-// Функция - обработчик события нажатия на кнопку поиска. 
+// Функция - обработчик события нажатия на кнопку поиска.
 async function onSubmit(event) {
-
   // Отменяем действие по умолчанию и сбрасываем галерею с кнопкой загрузки.
   event.preventDefault();
   refs.gallery.innerHTML = '';
   refs.loadBtn.classList.add('is-hidden');
 
-
-  // Проверяем значеник в поле поиска с параменту поиска в API сервисе.Выполняем блок кода, если значение в поле поиска новое.
+  // Проверяем значение в поле поиска с параметром поиска в API сервисе.Выполняем блок кода, если значение в поле поиска новое.
   try {
     if (myApiService.query !== event.currentTarget.elements.searchQuery.value) {
       myApiService.query = event.currentTarget.elements.searchQuery.value;
       myApiService.page = 1;
-      await validationOfOutcomingResult(myApiService, true, true);
+      validationOfOutcomingResult(myApiService, true, true);
       myApiService.lastPage === 1 && endOfGalleryNotification();
       myApiService.page += 1;
       return;
@@ -45,92 +42,96 @@ async function onSubmit(event) {
     else {
       if (myApiService.page >= myApiService.lastPage) {
         myApiService.page = myApiService.lastPage;
-        await validationOfOutcomingResult(myApiService);
-        await endOfGalleryNotification();
-        return
-      }
-      else {
-        await validationOfOutcomingResult(myApiService);
+        validationOfOutcomingResult(myApiService);
+        endOfGalleryNotification();
+        return;
+      } else {
+        validationOfOutcomingResult(myApiService);
         myApiService.page += 1;
-        return
+        return;
       }
     }
   } catch (error) {
     if (error instanceof MyError) {
-            Notify.failure(error.message);
-        }
-        else if (error instanceof TypeError) {
-            Notify.failure("Expected JSON response but got " + type);
-        }
-        else {
-            Notify.failure(error.message);
-        }
+      Notify.failure(error.message);
+    } else if (error instanceof TypeError) {
+      Notify.failure('Expected JSON response but got ' + type);
+    } else {
+      Notify.failure(error.message);
+    }
   }
 }
 
-// Функция - обработчик кнопки подгрузки изображений. Делаем плавный скролл 
+// Функция - обработчик кнопки подгрузки изображений. Делаем плавный скролл
 async function onLoadMore() {
   refs.formEl.reset();
-  myApiService.fetchData().then(data => renderImages(data.hits)).then(data => smoothScroll(data)).catch(error => Notify.failure(error.name));
+  myApiService
+    .fetchData()
+    .then(data => renderImages(data.hits))
+    .then(data => smoothScroll(data))
+    .catch(error => Notify.failure(error.name));
   myApiService.page === myApiService.lastPage && endOfGalleryNotification();
   myApiService.page += 1;
-  
 }
 
-
-// Функция уведомлений, в зависимости от количества найденных изображений. 
+// Функция уведомлений, в зависимости от количества найденных изображений.
 function notificationByFetchedResults(lengthOfResultedArray) {
-  const messageIfNotFounded = "Sorry, there are no images matching your search query. Please try again.";
+  const messageIfNotFounded =
+    'Sorry, there are no images matching your search query. Please try again.';
   const messageIfFoundOne = `You are lucky! We found 1 image.`;
-  const messaheIfFoundedMany = `We found ${lengthOfResultedArray} images.`;
+  const messageIfFoundedMany = `We found ${lengthOfResultedArray} images.`;
 
   if (lengthOfResultedArray === 0) {
     Notify.failure(messageIfNotFounded);
   } else if (lengthOfResultedArray === 1) {
     Notify.success(messageIfFoundOne);
   } else {
-    Notify.success(messaheIfFoundedMany);
-  } 
+    Notify.success(messageIfFoundedMany);
+  }
 }
 
 // Функция отрисовки изображений в галерее. Принимает массив объектов изображений. Результат - разметка галереи.
 function renderImages(arrayOfImagesData) {
-    const markup = arrayOfImagesData.map(image => renderCardTpl({image})).join('');
+  const markup = arrayOfImagesData.map(image => renderCardTpl({ image })).join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
-  const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, refresh: true });
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    refresh: true,
+  });
 }
 
-
 /**
- * 
- * Функция вызывающая рендер в зависимости от параметров поиска. Если 
- * передан воторой параметр, то выведем необходимое уведомление, в зависимости от запроса.
- * Если передан третий параментр, , то зададим последнюю страцицу в параметр API объекта.
- * @param {Promise} data 
- * @param {Boolean} notify 
- * @param {Boolean} setLastPage 
+ *
+ * Функция вызывающая рендер в зависимости от параметров поиска. Если
+ * передан второй параметр, то выведем необходимое уведомление, в зависимости от запроса.
+ * Если передан третий параметр, , то зададим последнюю страницу в параметр API объекта.
+ * @param {Promise} data
+ * @param {Boolean} notify
+ * @param {Boolean} setLastPage
  * @returns {void}
  */
 
 async function validationOfOutcomingResult(data, notify, setLastPage) {
-    const fetchingRequest = await data.fetchData();      
-    await renderImages(fetchingRequest.hits);
-    fetchingRequest.totalHits > data.per_page && refs.loadBtn.classList.remove('is-hidden');
+  const fetchingRequest = await data.fetchData();
+  renderImages(fetchingRequest.hits);
+  fetchingRequest.totalHits > data.per_page && refs.loadBtn.classList.remove('is-hidden');
 
-    notify && notificationByFetchedResults(fetchingRequest.totalHits);
-    setLastPage && (myApiService.lastPage = findLastPage(fetchingRequest));
+  notify && notificationByFetchedResults(fetchingRequest.totalHits);
+  setLastPage && (myApiService.lastPage = findLastPage(fetchingRequest));
 }
 
 // Функция уведомления о последней странице по данному запросу. Скрывает кнопку подгрузки изображений.
 function endOfGalleryNotification() {
-  const message = "You have reached the end of search results"
+  const message = 'You have reached the end of search results';
   refs.loadBtn.classList.add('is-hidden');
   Notify.info(message);
   myApiService.page = 1;
 }
 
 function smoothScroll() {
-  const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-  console.log(cardHeight);
-  window.scrollBy({top: cardHeight*3, behavior: 'smooth'});
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({ top: cardHeight * 3, behavior: 'smooth' });
 }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -2,51 +2,49 @@ import MyError from './myError';
 
 export default class NewsApiService {
   constructor() {
-        this.searchQuery = '';
-        this.KEY = '27097192-0671b14f562b4984cf83e0bbe';
-        this.IMAGE_TYPE = 'photo';
-        this.ORIENTATION = 'horizontal';
-        this.SAFESEARCH = 'true';
-        this.BASE_URL = 'https://pixabay.com/api/';
-        this.page = 1;
-      this.per_page = 20;
-      this.lastPage = 1;
+    this.searchQuery = '';
+    this.KEY = '27097192-0671b14f562b4984cf83e0bbe';
+    this.IMAGE_TYPE = 'photo';
+    this.ORIENTATION = 'horizontal';
+    this.SAFESEARCH = 'true';
+    this.BASE_URL = 'https://pixabay.com/api/';
+    this.page = 1;
+    this.per_page = 20;
+    this.lastPage = 1;
+  }
+
+  getOptions() {
+    const searchParams = new URLSearchParams({
+      key: this.KEY,
+      q: this.searchQuery,
+      image_type: this.IMAGE_TYPE,
+      orientation: this.ORIENTATION,
+      safesearch: this.SAFESEARCH,
+      page: this.page,
+      per_page: this.per_page,
+    });
+
+    return searchParams;
+  }
+
+  async fetchData() {
+    const res = await fetch(`${this.BASE_URL}?${this.getOptions()}`);
+    if (res.status === 400) {
+      throw new MyError('Sorry, there are no images matching your search query. Please try again.');
     }
-    
-    getOptions() {
-        const searchParams = new URLSearchParams({
-            key: this.KEY,
-            q: this.searchQuery,
-            image_type: this.IMAGE_TYPE,
-            orientation: this.ORIENTATION,
-            safesearch: this.SAFESEARCH,
-            page: this.page,
-            per_page: this.per_page
-        });
-
-        return searchParams;
-    }     
-
-    fetchData() {
-        return fetch(`${this.BASE_URL}?${this.getOptions()}`).then(res => {
-            if (res.status === 400) {
-                throw new MyError("Sorry, there are no images matching your search query. Please try again.");
-            }
-
-            let type = res.headers.get('Content-Type');
-            if (type !== 'application/json') {
-                throw new TypeError();
-            }
-        return res.json();
-        }).then(data => (console.log(data), data))
+    let type = res.headers.get('Content-Type');
+    if (type !== 'application/json') {
+      throw new TypeError();
     }
-    
-    get query() {
-        return this.searchQuery;
-    }
+    const data = await res.json();
+    return data;
+  }
 
-    set query(value) {
-        this.searchQuery = value;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
+  set query(value) {
+    this.searchQuery = value;
+  }
 }
